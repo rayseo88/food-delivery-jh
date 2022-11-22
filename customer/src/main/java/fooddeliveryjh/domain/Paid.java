fee5eb9ae6cbe7a12e9cbd52acc15420f23d49ee
rayseo88 <118163383+rayseo88@users.noreply.github.com>
2022-11-22 10:54:58 +0900

    commit

--- customer/src/main/java/fooddeliveryjh/domain/Paid.java
+++ customer/src/main/java/fooddeliveryjh/domain/Paid.java
@@ -1,12 +1,15 @@
 package fooddeliveryjh.domain;
 
+import fooddeliveryjh.domain.*;
 import fooddeliveryjh.infra.AbstractEvent;
-import lombok.Data;
+import lombok.*;
 import java.util.*;
-
 @Data
+@ToString
 public class Paid extends AbstractEvent {
 
     private Long id;
     private Long orderid;
 }
+
+
--- customer/src/main/java/fooddeliveryjh/domain/StoreAccepted.java
+++ customer/src/main/java/fooddeliveryjh/domain/StoreAccepted.java
@@ -1,16 +1,19 @@
 package fooddeliveryjh.domain;
 
+import fooddeliveryjh.domain.*;
 import fooddeliveryjh.infra.AbstractEvent;
-import lombok.Data;
+import lombok.*;
 import java.util.*;
-
 @Data
+@ToString
 public class StoreAccepted extends AbstractEvent {
 
     private Long id;
     private String foodId;
     private Long orderId;
     private String option;
-    private Address address;
+    private Object address;
     private String status;
 }
+
+
